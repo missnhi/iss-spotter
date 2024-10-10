14d11077aa87d3543319625b5a3a774447fffb98
missnhi <nhi.phan.ley@gmail.com>
2024-10-10 11:03:59 -0600

    fetch my geo locations from my IP address

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 const {fetchMyIP} = require('./iss');
+const {fetchCoordsByIP} = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,3 +10,11 @@ const {fetchMyIP} = require('./iss');
 //
 //   console.log('It worked! Returned IP:', ip);
 // });
+fetchCoordsByIP('104.205.4.46', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+  
+  console.log('It worked! Returned coordinates:', coordinates);
+});
--- iss.js
+++ iss.js
@@ -11,21 +11,46 @@ const needle = require('needle');
 const fetchMyIP = function(callback) {
   // use needle to fetch IP address from JSON API
   needle.get('https://api.ipify.org?format=json', (error, response) => {
+    // if there is an error, pass it to the callback
+    if (error) {
+      callback(error, null);
+    }
+    
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${response.body}`;
       callback(Error(msg), null);
       return;
     }
-    // if there is an error, pass it to the callback
-    if (error) {
-      callback(error, null);
-    } else {
-      //parse and extract the IP address using JSON and then pass that through to the callback
-      const ip = response.body.ip;
-      callback(null, ip);
-    }
+    
+    // if all went well, pass the data to the callback
+    //parse and extract the IP address using JSON and then pass that through to the callback
+    const ip = response.body.ip;
+    callback(null, ip);
   });
 }
 
+const fetchCoordsByIP = function(ip, callback) {
+  // use ipwho.is APIs to fetch coordinates from IP address
+  needle.get(`https://ipwho.is/${ip}`, (error, response) => {
+    // if there is an error, pass it to the callback
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    if (!response.body.success) {
+      const message = `Success status was ${response.body.success}. Server message says: ${response.body.message} when fetching for IP ${response.body.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    
+    // if all went well, pass the data to the callback
+    const {latitude, longitude} = response.body;
+    callback(null, {latitude, longitude});
+  });
+}
+
+
 module.exports = {fetchMyIP};
+module.exports = {fetchCoordsByIP};
